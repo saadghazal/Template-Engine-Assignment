a7a564424bd4003c3d269c1c57f571910b8b6806
saadghazal <saad.m.ghazal@icloud.com>
2023-12-13 20:21:25 +0300

    Setting Project Configurations

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000);
