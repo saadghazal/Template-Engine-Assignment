60815132ec67724880502c825563fcea9823004c
saadghazal <saad.m.ghazal@icloud.com>
2023-12-13 21:00:34 +0300

    Create Home Page

--- app.js
+++ app.js
@@ -9,4 +9,7 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+app.use(homeRoute)
+
 app.listen(3000);
