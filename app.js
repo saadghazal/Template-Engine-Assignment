4ca89459625f9c09a74e6892e51f2b4081d4f628
saadghazal <saad.m.ghazal@icloud.com>
2023-12-13 20:26:27 +0300

    Create Home Route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const homeRoute = require("./routes/home");
 const app = express();
 
 app.set("view engine", "ejs");
